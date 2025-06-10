d85e4e0373d68d06240bbfda6a7fa56d747161d1
BryanGonzalez262 <bsg262@gmail.com>
2025-06-10 14:46:28 -0400

    cleaned up EEG processing. will now include some manual artifact removal

--- src/MOBI_QC/eeg.py
+++ src/MOBI_QC/eeg.py
@@ -20,54 +20,6 @@ def compute_eeg_pipeline(xdf_filename):
     Args:
         xdf_filename (str): The path to the xdf file.
     """    
-    subject = xdf_filename.split('-')[1].split('/')[0]
-    df = get_event_data(event='RestingState', 
-                        df=import_eeg_data(xdf_filename),
-                        stim_df=import_stim_data(xdf_filename))
-
-    
-    ch_names = [f"E{i+1}" for i in range(df.shape[1] - 1)]
-    info = mne.create_info(ch_names, 
-                        sfreq=1/df.lsl_time_stamp.diff().mean(), 
-                        ch_types='eeg')
-    df.drop(columns=['lsl_time_stamp'], inplace=True)
-
-    raw = mne.io.RawArray(df.T * 1e-6, info=info) # multiplying by 1e-6 converts to volts
-
-    # Create a Cz reference
-    value = np.zeros((1, raw.n_times))
-    info = mne.create_info(["Cz"], raw.info['sfreq'], ch_types='eeg')
-    cz = mne.io.RawArray(value, info)
-    raw.add_channels([cz], force_update_info=True)
-
-    # Apply a montage
-    montage = mne.channels.make_standard_montage('GSN-HydroCel-129')
-    raw.set_montage(montage, on_missing='ignore')
-
-    raw.crop(tmin=0, tmax=5)
-    
-    prep_params = {
-            "ref_chs": "eeg",
-            "reref_chs": "eeg",
-            "line_freqs": np.arange(60, raw.info["sfreq"] / 2, 60),
-        }
-    # these params set up the robust reference  - i.e. median of all channels and interpolate bad channels
-    prep = pyprep.PrepPipeline(raw, montage=montage, channel_wise=True, prep_params=prep_params)
-    prep_output = prep.fit()
-    raw_cleaned = prep_output.raw_eeg
-
-    
-    vars = {}
-    print(f"Bad channels before robust reference: {prep.noisy_channels_original['bad_all']}")
-    vars['bad_channels_before'] = prep.noisy_channels_original['bad_all']
-    print(f"Interpolated channels: {prep.interpolated_channels}")
-    vars['interpolated_channels'] = prep.interpolated_channels
-    print(f"Bad channels after interpolation: {prep.still_noisy_channels}")
-    vars['bad_channels_after'] = prep.still_noisy_channels
-    
-    fig = raw_cleaned.plot_psd(tmax=np.inf, fmax=250)
-    fig.figure.savefig(f'report_images/{subject}_eeg_psd.png')
-    
     def annotate_blinks(
         raw: mne.io.Raw, ch_name: list[str] = ["E25", "E8"]
     ) -> mne.Annotations:
@@ -102,60 +54,93 @@ def compute_eeg_pipeline(xdf_filename):
             )
     
         return muscle_annotations
+    subject = xdf_filename.split('sub-')[-1].split('_')[0]
+
+    if len(glob('/'.join(xdf_filename.split('/')[:-1]) +'/*.fif')) < 1:
+        df = get_event_data(event='RestingState', 
+                            df=import_eeg_data(xdf_filename),
+                            stim_df=import_stim_data(xdf_filename))
+
+        ch_names = [f"E{i+1}" for i in range(df.shape[1] - 1)]
+        info = mne.create_info(ch_names, 
+                            sfreq=1/df.lsl_time_stamp.diff().mean(), 
+                            ch_types='eeg')
+        df.drop(columns=['lsl_time_stamp'], inplace=True)
+
+        raw = mne.io.RawArray(df.T * 1e-6, info=info) # multiplying by 1e-6 converts to volts
+
+        # Create a Cz reference
+        value = np.zeros((1, raw.n_times))
+        info = mne.create_info(["Cz"], raw.info['sfreq'], ch_types='eeg')
+        cz = mne.io.RawArray(value, info)
+        raw.add_channels([cz], force_update_info=True)
+
+        # Apply a montage
+        montage = mne.channels.make_standard_montage('GSN-HydroCel-129')
+        raw.set_montage(montage, on_missing='ignore')
+
+        #raw.crop(tmin=0, tmax=5)
+
+        prep_params = {
+                "ref_chs": "eeg",
+                "reref_chs": "eeg",
+                "line_freqs": np.arange(60, raw.info["sfreq"] / 2, 60),
+            }
+        # these params set up the robust reference  - i.e. median of all channels and interpolate bad channels
+        prep = pyprep.PrepPipeline(raw, montage=montage, channel_wise=True, prep_params=prep_params)
+        prep_output = prep.fit()
+        raw_cleaned = prep_output.raw_eeg
+
+
+        # check if cleaned file already exists
+        save_path = '/'.join(xdf_filename.split('/')[:-1]) + f'/sub-{subject}_ses-S001_task-CUNY_run-001_eeg_clean.fif'
+        raw_cleaned.save(save_path, overwrite=True)
+        vars = {}
+        print(f"Bad channels before robust reference: {prep.noisy_channels_original['bad_all']}")
+        vars['bad_channels_before'] = prep.noisy_channels_original['bad_all']
+        print(f"Interpolated channels: {prep.interpolated_channels}")
+        vars['interpolated_channels'] = prep.interpolated_channels
+        print(f"Bad channels after interpolation: {prep.still_noisy_channels}")
+        vars['bad_channels_after'] = prep.still_noisy_channels
+        # safe the vars dictionary to a file
+        with open('/'.join(xdf_filename.split('/')[:-1]) + f'/sub-{subject}_ses-S001_task-CUNY_run-001_eeg_clean_vars.json', 'w') as f:
+            json.dump(vars, f, indent=4)
+    else:
+        raw_cleaned = mne.io.read_raw_fif('/'.join(xdf_filename.split('/')[:-1]) + f'/sub-{subject}_ses-S001_task-CUNY_run-001_eeg_clean.fif', preload=True)
+        # read the vars dictionary from the file
+        with open('/'.join(xdf_filename.split('/')[:-1]) + f'/sub-{subject}_ses-S001_task-CUNY_run-001_eeg_clean_vars.json', 'r') as f:
+            vars = json.load(f)
+        print(f"Bad channels before robust reference: {vars['bad_channels_before']}")
+        print(f"Interpolated channels: {vars['interpolated_channels']}")
+        print(f"Bad channels after interpolation: {vars['bad_channels_after']}")
     
-    
-    # Applying a high pass filter to remove low frequency noise
-    raw_cleaned.filter(l_freq=0.5, h_freq=None)
-    fig = raw_cleaned.plot(show_scrollbars=False,
-                        show_scalebars=False,events=None, start=0, duration=300,n_channels=75, scalings=11e-5,color='k')
-    fig.figure.savefig(f'report_images/{subject}_eeg_annotations.png')
-  
-    
+    # Annotate blinks and muscle artifacts to compute percent good data
     blink_annotations = annotate_blinks(raw_cleaned, ch_name=["E25", "E8"])
-
     muscle_annotations = annotate_muscle(raw_cleaned)
-
-    all_annotations = blink_annotations + muscle_annotations + raw.annotations
+    all_annotations = blink_annotations + muscle_annotations + raw_cleaned.annotations
     raw_cleaned.set_annotations(all_annotations)
-    
     # Create a binary array
     binary_mask = np.zeros(len(raw_cleaned.times), dtype=int)
-
     # Iterate over annotations
     for annot in raw_cleaned.annotations:
         onset_sample = int(annot['onset'] * raw_cleaned.info['sfreq'])
         duration_sample = int(annot['duration'] * raw_cleaned.info['sfreq'])
         binary_mask[onset_sample:onset_sample + duration_sample] = 1
-
     percent_good = 1 - np.sum(binary_mask) / len(binary_mask)
     print(f'Percent Good Data: {percent_good * 100:.2f}%')
     vars['percent_good'] = percent_good * 100
-    
-    #filt_raw = raw.copy().filter(l_freq=1.0, h_freq=None)
-    filt_raw = raw_cleaned.copy()
-    filt_raw.info['bads'] = prep.still_noisy_channels 
-    ica = mne.preprocessing.ICA(n_components=.99, method='picard')
-    ica.fit(filt_raw)
-    #ica.plot_sources(filt_raw)
-    fig = ica.plot_components()#.savefig(f'report_images/{subject}_ica_components.png')
-    for i, f in enumerate(fig):
-        f.savefig(f'report_images/{subject}_ica_components_{i}.png')
-        plt.close('all')
-    #plt.savefig(f'report_images/{subject}_ica_components.png')
-    comp_idx, scores = ica.find_bads_muscle(filt_raw)
 
-    # Remove the muscle artifacts
-    raw_ica = ica.apply(filt_raw, exclude=comp_idx)
+    # set notch filter
+    raw_cleaned.notch_filter(60)
+    # set bandpass filter
+    raw_cleaned.filter(l_freq=1.0, h_freq=50.0) # only keeping frequencies between 1-50 Hz
+    # play around with this number to get components 
+    # that seem to represent the actual brain activations well
+    num_components = .95 
+    ica = ICA(n_components=num_components, method='picard')
+    ica.fit(raw_cleaned)
 
-    
-    eog_evoked = mne.preprocessing.create_eog_epochs(raw_cleaned, ch_name=['E8', 'E25']).average(picks="all")
-    eog_evoked.apply_baseline((None, None))
-    eog_evoked.plot_joint().savefig(f'report_images/{subject}_eog_evoked.png')
-    
-    eog_projs, _ = mne.preprocessing.compute_proj_eog(raw_cleaned, n_eeg=1, reject=None, no_proj=True,
-                                                    ch_name=['E8', 'E25'])
-
-    return vars
+    return vars, raw_cleaned, ica
 
 
 def test_eeg_pipeline(xdf_filename):
