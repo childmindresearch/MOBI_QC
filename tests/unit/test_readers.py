bad9a7af5535b920fe48dcd3ab358a061a188c78
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2025-10-21 10:59:57 -0400

    Update test_readers.py
    Added a more concrete example

--- tests/unit/test_readers.py
+++ tests/unit/test_readers.py
@@ -2,5 +2,14 @@
 
 
 def test_placeholder() -> None:
-    """Placeholder test until real tests are implemented."""
-    assert True
+    """Placeholder test until real tests are implemented.
+
+    Tests should cover one discreet unit of code. Ideally, these tests should cover both
+    the basic functionality as well as edge cases and failure modes.
+
+    Tests should follow the Arrange-Act-Assert pattern.
+    """
+    expected_result = 2.0
+
+    result = 1.0 + 1.0
+    assert result == expected_result
