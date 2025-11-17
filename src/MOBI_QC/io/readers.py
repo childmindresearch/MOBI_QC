9e17b0fdd518097a22d2ab30a2fdc1e25b2f95ee
BryanGonzalez262 <bsg262@gmail.com>
2025-11-17 14:31:58 -0500

    updated readers and poetry.lock/ pyproject.toml

--- src/MOBI_QC/io/readers.py
+++ src/MOBI_QC/io/readers.py
@@ -1,29 +1,31 @@
 """This module contains functions to read MOBI xdf files."""
-import pyxdf
 import pathlib
 from typing import Optional
 
+import pyxdf
 
-def read_xdf(xdf_path: str | pathlib.Path, stream_names: Optional[list[str]] = None) -> list:
-    """Reads a MOBI xdf file and returns the data streams.
 
-    Parameters
+def read_xdf(xdf_path: str | pathlib.Path, 
+             stream_names: Optional[list[str]] = None) -> list:
+    """Reads a MOBI xdf file and returns a list containing the data streams.
+
+    Args:
     ----------
-    xdf_path : str
+    xdf_path : 
         Path to the MOBI xdf file.
-    stream_name : list[str], optional
+    stream_names : 
         List of stream names to read from the xdf file. If None, all streams are read.
 
-    Returns
+    Returns:
     -------
     list
         A list of data streams contained in the xdf file.
     """
     if xdf_path is None:
         raise ValueError("XDF path must be provided")
-    if not isinstance(xdf_path, str):
-        raise TypeError("XDF path must be a string")
-    if not xdf_path.endswith('.xdf'):
+    if not isinstance(xdf_path, str | pathlib.Path):
+        raise TypeError("XDF path must be a string or pathlib.Path")
+    if not str(xdf_path).endswith('.xdf'):
         raise ValueError("XDF path must end with '.xdf'")  
     
     if stream_names is not None and not isinstance(stream_names, list):
@@ -40,11 +42,10 @@ def read_xdf(xdf_path: str | pathlib.Path, stream_names: Optional[list[str]] = N
 
     else:
         streams, _ = pyxdf.load_xdf(xdf_path, 
-                                    select_streams=[{"name": name} for name in stream_names],
+                                    select_streams=[{"name": name} 
+                                                    for name in stream_names],
                                     verbose=False)
         
     return streams
 
 
-if __name__ == "__main__":
-    pass
