f828eaf7493147e6bfda67cc96a13ad29a2beec7
BryanGonzalez262 <bsg262@gmail.com>
2025-11-25 15:05:58 -0500

    reformat with ruff

--- src/MOBI_QC/io/readers/readers.py
+++ src/MOBI_QC/io/readers/readers.py
@@ -1,19 +1,19 @@
 """This module contains functions to read MOBI xdf files."""
+
 import pathlib
 from typing import Optional
 
 import pyxdf
 
 
-def read_xdf(xdf_path: str | pathlib.Path, 
-             stream_names: Optional[list[str]] = None) -> list:
+def read_xdf(xdf_path: str | pathlib.Path, stream_names: Optional[list[str]] = None) -> list:
     """Reads a MOBI xdf file and returns a list containing the data streams.
 
     Args:
     ----------
-    xdf_path : 
+    xdf_path :
         Path to the MOBI xdf file.
-    stream_names : 
+    stream_names :
         List of stream names to read from the xdf file. If None, all streams are read.
 
     Returns:
@@ -33,20 +33,15 @@ def read_xdf(xdf_path: str | pathlib.Path,
     xdf_path = pathlib.Path(xdf_path)
 
     if xdf_path.suffix != ".xdf":
-        raise ValueError("XDF path must end with '.xdf'")  
-    
+        raise ValueError("XDF path must end with '.xdf'")
+
     if not xdf_path.exists():
         raise FileNotFoundError(f"XDF file not found: {xdf_path}")
-    
+
     if stream_names is None:
         streams, _ = pyxdf.load_xdf(xdf_path)
 
     else:
-        streams, _ = pyxdf.load_xdf(xdf_path, 
-                                    select_streams=[{"name": name} 
-                                                    for name in stream_names],
-                                    verbose=False)
-        
+        streams, _ = pyxdf.load_xdf(xdf_path, select_streams=[{"name": name} for name in stream_names], verbose=False)
+
     return streams
-
-
