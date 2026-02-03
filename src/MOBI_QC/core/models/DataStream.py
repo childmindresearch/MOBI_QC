4920a4191c515f09adfb772518a3fe1238c114d8
camillastrauss <camilla.k.strauss@gmail.com>
2026-02-03 15:06:25 -0500

    ruff reformat again

--- src/MOBI_QC/core/models/DataStream.py
+++ src/MOBI_QC/core/models/DataStream.py
@@ -80,9 +80,9 @@ class DataStream:
         Recalculates sampling rate based on filtered data.
 
         DataStream.data will be empty if onset_timestamp is greater than the
-        last value of data['time_stamp'], if offset_timestamp is less than the first
-        value of data['time_stamp'], or if onset_timestamp and offset_timestamp are 
-        in between two values of data['time_stamp'].
+        last value of data['time_stamp'], if offset_timestamp is less than the
+        first value of data['time_stamp'], or if onset_timestamp and offset_timestamp
+        are in between two values of data['time_stamp'].
 
         Args:
             onset_timestamp: start time (seconds) for filtering the data
