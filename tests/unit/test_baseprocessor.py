d87f3020ff18b5c5c158b16ef5ccf8bbacc4ff2a
BryanGonzalez262 <bsg262@gmail.com>
2026-02-11 14:58:36 -0500

    new time test

--- tests/unit/test_baseprocessor.py
+++ tests/unit/test_baseprocessor.py
@@ -48,7 +48,7 @@ def test_baseprocessor_format_data(sample_xdf_file: str | pathlib.Path) -> None:
 
 def test_get_collection_date(sample_xdf_file: str | pathlib.Path) -> None:
     """Test the get_collection_date method of BaseProcessor."""
-    expected_date = "2026-02-11 13:57:37"#"2026-01-14 16:08:07"
+    expected_date = "2026-02-11 19:46:04" #"2026-02-11 13:57:37"#"2026-01-14 16:08:07"
     processor = BaseProcessor.BaseProcessor(xdf_path=sample_xdf_file)
     collection_date = processor.get_collection_date()
 
